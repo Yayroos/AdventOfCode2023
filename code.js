3558dbcbb860d8ed6f32ceeea7cbe7484c156a0f
Yayroos <yayroos@protonmail.com>
2023-12-02 11:10:56 +1100

    day 1 and templating for future

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 const input = require('./input.js');
+
+
+const lines = input.split("\n");
+
+//part 1
+function part1(){
+    
+}
+
+function part2(){
+    
+}
+
+console.log("Part 1", part1());
+console.log("Part 2", part2());
